cb06beffd7fe70241dc2279275f69d5c95083178
Karan Kumar <karan@couchbase.com>
2012-07-05 16:21:00 -0700

    More refactoring of the code
    rules defined
    classifying tests broadly

--- get-test-data.py
+++ get-test-data.py
@@ -5,9 +5,16 @@ import string
 from lxml import etree
 import StringIO
 
-sender = 'qe@couchbase.com'
+sender = 'karan@couchbase.com'
 receivers = ['karan@couchbase.com']
 mail_server = '10.1.0.118'
+results = {}
+threshold = 0.15
+
+single_node_xmls = ['memcapable' , 'expirytests', 'drainratetests', 'deletebuckettests',
+                    'setgettests', 'createbuckettests', 'recreatebuckettests']
+
+test_classes = ['rebalance', 'swaprebalance', 'view', 'xdcr', 'warmup', 'failover', 'basic']
 
 def get_build_doc(db, build):
     doc_id = ''
@@ -26,6 +33,145 @@ def get_build_doc(db, build):
         exit(1)
     return doc
 
+def get_detailed_status(options):
+    total_tests = 0
+    total_errors = 0
+    server = Server(options.node)
+    db = server.get_db(options.database)
+    doc = get_build_doc(db, options.build)
+    doc_content = db.open_doc(doc['_id'])
+    tests_list = doc_content['_attachments'].keys()
+    # TODO: Check for number of tests that ran
+    print "List of tests against %s are %s " % (options.build, tests_list)
+    failed_tests = []
+    # Data struct to store information per test_class (attachment)
+    # {test_name:{tests:<int>, errros:<int>, time:<float>}}
+    test_data = {}
+    for attachment, value in doc_content['_attachments'].items():
+        errors_count = 0
+        tests_count = 0
+        print "Fetching attachment %s " % attachment
+        file = db.fetch_attachment(doc, attachment)
+        file = file.encode('ascii', 'ignore')
+        xmldoc =  etree.parse(StringIO.StringIO(file))
+        root = xmldoc.getroot()
+        for child in root:
+            attributes = child.attrib
+            for childish in child:
+                # To print the error
+                #print childish.text
+                failed_tests.append(attributes.get('name'))
+        attributes = root.attrib
+        # To get root element attributes
+        #print attributes
+        name = attributes.get('name')
+        errors_count = int(attributes.get('errors'))
+        tests_count = int(attributes.get('tests'))
+        total_tests += tests_count
+        total_errors += errors_count
+        print "Testname: %s, passed: %s, failed: %s" % (name, tests_count-errors_count,
+                                                        errors_count)
+        test_data[name] = {}
+        test_data[name]['tests'] = tests_count
+        test_data[name]['errors'] = errors_count
+        test_data[name]['time'] = float(attributes.get('time'))
+
+    print test_data
+    text = "Passed %s out of %s tests on %s build\n" % (total_tests-total_errors, total_tests,
+                                                        options.build)
+    num_failed = len(failed_tests)
+    if num_failed > 0:
+        text += "\nList of Failed tests:\n"
+        for i in range(num_failed):
+            text += "%s: %s\n" % (i+1, failed_tests[i])
+
+    print text
+    return test_data
+
+def initialize_results():
+    for test in test_classes:
+        results[test] = {}
+        results[test]['tests'] = 0
+        results[test]['errors'] = 0
+        results[test]['time'] = 0
+        results[test]['status'] = 'None'
+
+def update_results(key, data):
+    if key is '':
+        return
+    results[key]['tests'] += data['tests']
+    results[key]['errors'] += data['errors']
+    results[key]['time'] += data['time']
+
+def rules_engine(data={}):
+    initialize_results()
+    # Groupings
+    for test, data in data.items():
+        key = ''
+        if test.startswith('rebalance') or test.startswith('rebalancetests'):
+            key = 'rebalance'
+        elif test.startswith('swaprebalance'):
+            key = 'swaprebalance'
+        elif test.startswith('view'):
+            key = 'view'
+        elif test.startswith('xdcr'):
+            key = 'xdcr'
+        elif test.startswith('failover'):
+            key = 'failover'
+        elif test.startswith('warmup'):
+            key = 'warmup'
+        else:
+            key = 'basic'
+        update_results(key, data)
+
+def get_trunk_status():
+    for test, data in results.items():
+        if data['errors'] > threshold *  data['tests']:
+            data['status'] = 'RED'
+        else:
+            data['status'] = 'GREEN'
+
+def send_email(build):
+
+    num_tests = len(test_classes)
+    text = ''
+    red_count = 0
+    totals = 0
+    errors = 0
+    build_status = 'GREEN'
+    if num_tests > 0:
+        text += "\nTests in detail (centos 64 bit):\n"
+        for i in range(num_tests):
+            test = test_classes[i]
+            status = results[test]['status']
+            #text += "%s: %8s\n" % (test, status)
+            text += "{0}: {1:8s}\n".format(test, status)
+            totals += results[test]['tests']
+            errors += results[test]['errors']
+
+
+    if results['basic']['status'] is 'RED':
+        build_status = 'RED'
+    if errors > totals* threshold:
+        build_status = 'RED'
+
+    print text
+    try:
+        BODY = string.join((
+            "From: %s" % sender,
+            "To: %s" % receivers,
+            "Subject: %s: %s: %s" % ('2.0.0 trunk status', build, build_status) ,
+            "", text
+            ), "\r\n")
+
+        smtpObj = smtplib.SMTP(mail_server)
+        smtpObj.sendmail(sender, receivers, BODY)
+        print "Successfully sent email"
+        smtpObj.quit()
+    except Exception as ex:
+        print ex.message
+        print "Error: unable to send email"
+
 
 if __name__ == "__main__":
     parser = OptionParser()
@@ -40,60 +186,10 @@ if __name__ == "__main__":
     options, args = parser.parse_args()
 
     try:
-        total_tests = 0
-        total_errors = 0
-        server = Server(options.node)
-        db = server.get_db(options.database)
-        doc = get_build_doc(db, options.build)
-        doc_content = db.open_doc(doc['_id'])
-        print "List of tests against %s are %s " % (options.build, doc_content['_attachments'].keys())
-        failed_tests = []
-        for attachment, value in doc_content['_attachments'].items():
-            error_count = 0
-            tests_count = 0
-            print "Fetching attachment %s " % attachment
-            file = db.fetch_attachment(doc, attachment)
-            file = file.encode('ascii', 'ignore')
-            xmldoc =  etree.parse(StringIO.StringIO(file))
-            root = xmldoc.getroot()
-            for child in root:
-                attributes = child.attrib
-                for childish in child:
-                    # To print the error
-                    #print childish.text
-                    failed_tests.append(attributes.get('name'))
-            attributes = root.attrib
-            print attributes
-
-            error_count = int(attributes.get('errors'))
-            tests_count = int(attributes.get('tests'))
-            total_tests += tests_count
-            total_errors += error_count
-            print "Testname: %s, passed: %s, failed: %s" % (attributes.get('name'), tests_count-error_count,
-                                                            error_count)
-
-        text = "Passed %s out of %s tests on %s build" % (total_tests-total_errors, total_tests,
-                                                          options.build)
-        print text
-        print "Failed Tests:"
-        print failed_tests
-        try:
-
-            BODY = string.join((
-                "From: %s" % sender,
-                "To: %s" % receivers,
-                "Subject: %s: %s: " % ('2.0.0 trunk status', options.build) ,
-                "",
-                text
-                ), "\r\n")
-
-            smtpObj = smtplib.SMTP(mail_server)
-            smtpObj.sendmail(sender, receivers, BODY)
-            print "Successfully sent email"
-            smtpObj.quit()
-        except Exception as ex:
-            print ex.message
-            print "Error: unable to send email"
+        test_data = get_detailed_status(options)
+        rules_engine(test_data)
+        get_trunk_status()
+        send_email(options.build)
 
     except Exception as ex:
         print ex.message
